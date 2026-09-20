47c3f0d7143cc9824b36d43d5bdfd353caf226be
이태화 <itaehwa@10.nate.com>
2026-09-21 02:26:10 +0900

    fix: 힌트 버튼 크기

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
  *
  * 동기화 요청은 어느 쪽도 아니다. 손대지 않고 그냥 지나보낸다.
  */
-const V = "dotlog-v15";
+const V = "dotlog-v16";
 const DOC = "./index.html";
 const SHELL = ["./", DOC, "./products-catalog.js", "./manifest.webmanifest",
                "./icons/icon-192.png", "./icons/icon-512.png"];
